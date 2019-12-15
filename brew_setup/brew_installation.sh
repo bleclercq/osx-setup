9455560caea56d6b946e11a08efb2b3172f7e302
Brice Leclercq <brice.leclercq.pro@gmail.com>
2019-12-15 13:16:52 +0100

    Add a new test to handle cask installation with sudo

--- brew_setup/brew_installation.sh
+++ brew_setup/brew_installation.sh
@@ -4,6 +4,8 @@ GREEN="\033[0;32m"
 RED="\033[0;31m"
 COLOR_RESET="\033[0m"
 
+# TODO Add a global progress bar
+
 echo -e "${GREEN}Installing XCode${COLOR_RESET}"
 xcode-select --install
 
@@ -25,6 +27,10 @@ raw_list_of_nice_to_have_casks=$(cat ./casks/nice_to_have.txt | tr "\n" " ")
 list_of_nice_to_have_casks=($raw_list_of_nice_to_have_casks)
 nice_to_have "${list_of_nice_to_have_casks[@]}"
 
+raw_list_of_sudo_needed_casks=$(cat ./casks/nice_to_have.txt | tr "\n" " ")
+list_of_sudo_needed_casks=($raw_list_of_sudo_needed_casks)
+sudo_needed "${list_of_sudo_needed_casks[@]}"
+
 echo -e "${GREEN}Cleaning up${COLOR_RESET}"
 brew cleanup
 rm -rf "$(brew --cache)"
@@ -33,12 +39,12 @@ echo -e "${GREEN}Done${COLOR_RESET}"
 
 echo -e "The following formulaes had been installed through brew install:"
 brew list
-echo -e"\nIf you think you don't need some of them, run:\n"
-echo -e"brew uninstall <FORMULAE>\n"
-echo -e"\nThe following casks had been installed through brew cask install:"
+echo -e "\nIf you think you don't need some of them, run:\n"
+echo -e "brew uninstall <FORMULAE>\n"
+echo -e "\nThe following casks had been installed through brew cask install:"
 brew cask list
-echo -e"\nIf you think you don't need some of them, run:"
-echo -e"brew cask uninstall <CASK>"
+echo -e "\nIf you think you don't need some of them, run:"
+echo -e "brew cask uninstall <CASK>"
 echo -e "\nIf anything is missing, please search here:"
 echo -e "https://formulae.brew.sh/"
 echo -e "Here is another resourceful link, check it out:"
--- brew_setup/utilities/brew_fetch_and_install.sh
+++ brew_setup/utilities/brew_fetch_and_install.sh
@@ -6,17 +6,25 @@ source ./utilities/brew_fetchs.sh
 # TODO ADD A WAY TO BREW UPGRADE OUTDATED PACKAGES
 
 function formulaes_fetch_and_install {
-    raw_list_of_formulaes=$(find ./formulaes/ -type f ! -name 'nice_to_have.txt' | xargs cat)
+    raw_list_of_formulaes=$(find ./formulaes/ -type f ! -name "nice_to_have.txt" | xargs cat)
     list_of_formulaes=($raw_list_of_formulaes)
     brew_fetch_list "${list_of_formulaes[@]}" > /dev/null 2>&1
     brew_install_list "${list_of_formulaes[@]}" > /dev/null 2>&1
-    printf '%s ' "${list_of_formulaes[@]}" | tr "\n" " " | sort
+    printf "%s " "${list_of_formulaes[@]}" | tr "\n" " " | sort
 }
 
 function casks_fetch_and_install {
-    raw_list_of_cask=$(find ./casks/ -type f ! -name 'nice_to_have.txt' | xargs cat)
+    raw_list_of_cask=$(find ./casks/ -type f ! -name 'nice_to_have.txt' ! -name 'sudo_needed.txt' | xargs cat)
     list_of_casks=($raw_list_of_cask)
     brew_cask_fetch_list "${list_of_casks[@]}" > /dev/null 2>&1
     brew_cask_install_list "${list_of_casks[@]}" > /dev/null 2>&1
-    printf '%s ' "${list_of_casks[@]}" | tr "\n" " " | sort
+    printf "%s " "${list_of_casks[@]}" | tr "\n" " " | sort
+}
+
+function casks_sudo_fetch_and_install {
+    raw_list_of_cask=$(find ./casks/ -type f -name 'sudo_needed.txt' | xargs cat)
+    list_of_casks=($raw_list_of_cask)
+    brew_cask_fetch_list "${list_of_casks[@]}" > /dev/null 2>&1
+    brew_cask_install_list "${list_of_casks[@]}" > /dev/null 2>&1
+    printf "%s " "${list_of_casks[@]}" | tr "\n" " " | sort
 }
